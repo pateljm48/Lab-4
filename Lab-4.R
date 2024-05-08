93fbf4f95ca350a57011aab1e26a729cff719bc7
pateljm48 <146973820+pateljm48@users.noreply.github.com>
2024-05-08 18:02:18 -0400

    Add files via upload

---
+++ Lab-4.R
@@ -0,0 +1,27 @@
+set.seed(10923)
+
+# Number of students
+num_students <- 500
+
+# Enter code below:
+
+# Simulate study hours (ranging from 1 to 20 hours)
+study_hours <- sample(1:20, num_students, replace = TRUE)
+
+# Simulate quiz scores (ranging from 0 to 100)
+quiz_scores <- sample(0:100, num_students, replace = TRUE)
+
+# Simulate forum participation (ranging from 0 to 50 posts)
+forum_posts <- sample(0:50, num_students, replace = TRUE)
+
+# Simulate previous grades (ranging from 0 to 100)
+previous_grades <- sample(0:100, num_students, replace = TRUE)
+
+# Simulate final grades (ranging from 0 to 100)
+final_grades <- 0.3 * study_hours + 0.4 * quiz_scores + 0.2 * forum_posts + 0.1 * previous_grades + rnorm(num_students, mean = 0, sd = 5) + 25
+
+# Create a data frame
+student_data <- data.frame(StudyHours = study_hours, QuizScores = quiz_scores, ForumPosts = forum_posts, PreviousGrades = previous_grades, FinalGrades = final_grades)
+
+# View the first few rows of the generated data
+head(student_data)
